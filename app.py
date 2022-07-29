dbe7d21dd13397e0177473c99001cffc91d81313
ariksetyawan <setyawanarik@gmail.com>
2022-07-29 19:35:02 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+from neo4j import GraphDatabase, basic_auth
+
+# Graph DB Connection
+graph_user = "neo4j"
+graph_password = "0oIXkYwWMhaEfmccsTTQJeMz3nHScaPbrE451Cq28k0"
+graph_url = "neo4j+s://a0e76398.databases.neo4j.io:7687"
+
+driver = GraphDatabase.driver(graph_url, auth=basic_auth(graph_user, graph_password))
+session = driver.session()
+
+app = Flask(__name__)
+api = Api(app)
+
+# Custom Function
+# Token Check
+def check_token(Token):
+    user = Users.select().where(Users.Token == Token)
+    if user.exists():
+        user = user.dicts().get()
+
+        # cek expire
+        now = int(time.time())
+        if now > int(user['Expire_Token']):
+            return "Expire"
+        else:
+            return "Ok"
+    else:
+        return "Bad Token"
+
+def HeaderTokenVerification(header):
+    if "Token" in header:
+        token_status = check_token(header['Token'])
+        if token_status == 'Ok':
+            pass
+        elif token_status == 'Expire':
+            new_token = refresh_token(header['Token'])
+            data_return = {
+                "data":{"token":new_token},
+                "message":"Token Expired",
+                "code":"400",
+                "error":{
+                    "header":[{"params":"Token", "message":"Token Expired"}]
+                }
+            }
+            return jsonify(data_return)
+        else:
+            data_return = {
+                "data":None,
+                "message":"Token {}".format(token_status),
+                "code":"400",
+                "error":{
+                    "header":[{"params":"Token", "message":"Token {}".format(token_status)}]
+                }
+            }
+            return jsonify(data_return)
+    else:
+        data_return = {
+            "data":None,
+            "message":"Token Not Found",
+            "code":"400",
+            "error":{
+                "header":[{"params":"Token", "message":"Token Missing"}]
+            }
+        }
+        return jsonify(data_return)
+
+
+class Resource_Users(Resource):
+    def get(self):
+        # create_user = graph.run(" create (n:Users {Name:$Name}) ",Name="Alice")
+        query_user = session.run("match (n) return n")
+        for i in query_user:
+            # GET LABEL OF NODE
+            print(list(i.values()[0].labels)[0])
+        # print("createdr")
+        return jsonify({"message":"HelloWolrd"})
+
+
+api.add_resource(Resource_Users, '/api/users/')
+
+if __name__ == "__main__":
+	app.run(debug=True)
